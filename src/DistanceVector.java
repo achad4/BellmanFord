6ac0af7fe34a11386233f1533586ad0cb060deb6
Avi Chad-Friedman <ajc2212@columbia.edu>
2015-04-24 01:09:33 -0400

    Initial commit

---
+++ src/DistanceVector.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+/**
+ * Avi Chad-Friedman
+ * ajc2212
+ *
+ * Represents a distance vector to be transmitted between routers
+ */
+public class DistanceVector implements Serializable{
+}
